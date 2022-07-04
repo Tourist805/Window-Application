c79d42d9db5b4aa33930752416477404025ea16a
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-07-05 00:21:12 +0100

    Updating Models

---
+++ Engine/Models/SamplePerUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class SamplePerUser
+    {
+        public int SampleID { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Engine/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; } 
+    }
+}
